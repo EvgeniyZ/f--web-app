70088adced0d3c2b4591b285794ec7fb8c42b1c3
Evgenii Zakharov <eugene.zakharov93@gmail.com>
2020-12-20 20:34:01 +0300

    add UniversityDomain, add StudentsController

---
+++ Controllers/StudentsController.fs
@@ -0,0 +1,21 @@
+﻿namespace f__web_app.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+open UniversityDomain
+
+[<ApiController>]
+[<Route("[controller]")>]
+type StudentsController (logger : ILogger<StudentsController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() =
+        let students = new ResizeArray<Student>()
+        students.Add 
+        students
---
+++ UniversityDomain.fs
@@ -0,0 +1,35 @@
+﻿module UniversityDomain
+
+open System
+
+type StudentId = StudentId of int
+type AdmissionYear = AdmissionYear of DateTime
+type ReleaseYear = ReleaseYear of DateTime
+type StudentAge = private StudentAge of int
+
+module StudentAge =
+    let private minimumAge = 17
+    let private maximumAge = 120
+
+    let create age =
+        if age < minimumAge
+        then Error $"{nameof (StudentAge)} must be greater than {minimumAge}"
+        else if age > maximumAge
+        then Error $"{nameof (StudentAge)} must be less then {maximumAge}"
+        else Ok(StudentAge age)
+
+type CourseId = CourseId of int
+
+type Student =
+    { Id: StudentId
+      FirstName: string
+      LastName: string
+      Age: StudentAge
+      CourseId: CourseId
+      AdmissionYear: AdmissionYear
+      ReleaseYear: ReleaseYear }
+
+type Course =
+    { Id: CourseId
+      Name: string
+      StudentIds: StudentId list }
